f8dc9b409c82f7f719fac0eb05e586b1ab8bf6d1
Alexis Pavlidis <apavlidi@it.teithe.gr>
2020-05-12 17:48:58 +0300

    change eslint config

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: __dirname + '/dist'
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'public/index.html',
