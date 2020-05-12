056ea4e763c9d87b875d0fa1202bd76724b13510
Alexis Pavlidis <apavlidi@it.teithe.gr>
2020-05-12 17:34:48 +0300

    add material-ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import '../public/index.css';
+import Toolbar from "@material-ui/core/Toolbar";
+import AppBar from "@material-ui/core/AppBar";
+import Typography from "@material-ui/core/Typography";
 
 function App() {
     return (
         <div className="App">
-            <h1>Hi</h1>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6">
+                        Retropolis
+                    </Typography>
+                </Toolbar>
+            </AppBar>
         </div>
     );
 }
