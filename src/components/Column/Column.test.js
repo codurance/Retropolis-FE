68d72a8b1f832a21f6ad347e024c6f1d515da147
Tom <thomas.spencer@codurance.com>
2020-05-14 16:21:33 +0100

    Add passing render form test for Column

--- src/components/Column/Column.test.js
+++ src/components/Column/Column.test.js
@@ -9,3 +9,15 @@ it('should render the column', () => {
   const { getByText } = render(<Column />);
   getByText('Went well');
 });
+
+it('should render add card button', () => {
+  const { getByText } = render(<Column />);
+  getByText('Add card');
+});
+
+it('should render a form when add card button is clicked', () => {
+  const { getByText } = render(<Column />);
+  const cardButton = getByText('Add card');
+  cardButton.click();
+  getByText('Enter a title for this card...');
+});
