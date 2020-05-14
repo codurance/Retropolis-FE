ab5292fd3d3add1002dd710a859d6450b00ee294
Tom <thomas.spencer@codurance.com>
2020-05-14 14:54:43 +0100

    Add CardForm conditional rendering

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
@@ -6,21 +6,41 @@ import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
 import Typography from '@material-ui/core/Typography';
 import CardItem from '../CardItem/CardItem';
+import CardForm from '../CardForm/CardForm';
 
-const Column = () => (
-  <Card style={{ backgroundColor: '#f6f5f5' }}>
-    <CardContent>
-      <Typography variant="h5" gutterBottom>
-        Went well
-      </Typography>
-      <CardItem />
-      <CardItem />
-      <CardItem />
-    </CardContent>
-    <CardActions>
-      <Button style={{ textTransform: 'capitalize' }} size="small" startIcon={<AddIcon />}>Add card</Button>
-    </CardActions>
-  </Card>
-);
+const Column = () => {
+  const [cardFormEdit, setCardFormEdit] = useState(false);
+
+  return (
+    <Card style={{ backgroundColor: '#f6f5f5' }}>
+      <CardContent>
+        <Typography variant="h5" gutterBottom>
+          Went well
+        </Typography>
+        <CardItem />
+        <CardItem />
+        <CardItem />
+      </CardContent>
+      <CardActions>
+        { cardFormEdit
+          ? (
+            <CardForm />
+          )
+          : (
+            <Button
+              onClick={() => {
+                setCardFormEdit(!cardFormEdit);
+              }}
+              style={{ textTransform: 'capitalize' }}
+              size="small"
+              startIcon={<AddIcon />}
+            >
+              Add card
+            </Button>
+          )}
+      </CardActions>
+    </Card>
+  );
+};
 
 export default Column;
