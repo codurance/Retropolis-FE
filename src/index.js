dd4cf3fbfdbfd9f552355fbab15360420c6b40d7
Tom <thomas.spencer@codurance.com>
2020-05-14 16:45:23 +0100

    Delete extra files - not needed

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createGlobalStyle } from 'styled-components';
 import App from './App';
-
-const GlobalStyle = createGlobalStyle`
-  html {
-    background-color: white;
-    box-sizing: border-box;
-  }
-`;
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
