89bd925775afa4fad9951b12ac38570d99ccdeb8
Tom <thomas.spencer@codurance.com>
2020-05-18 11:35:34 +0100

    Prepare mock response for getBoards

---
+++ src/api/boardsApi.js
@@ -0,0 +1,15 @@
+const fetch = require('node-fetch');
+
+function handleResponse(response) {
+  if (response.ok) return response.json();
+  throw new Error(response.text());
+}
+
+async function getBoards() {
+  const response = await fetch('http://localhost:3000/src/api/boardApiMock.json');
+  return handleResponse(response);
+}
+
+getBoards().then(console.log);
+
+module.exports = { getBoards };
