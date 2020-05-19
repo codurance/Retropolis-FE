203f30d1c9a8b2f37e1c90c93a14471252172faa
Tom <thomas.spencer@codurance.com>
2020-05-19 16:46:59 +0100

    Correct uneccessary import

--- src/components/Board/Board.test.js
+++ src/components/Board/Board.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {jest, it, beforeAll} from '@jest/globals';
+import { jest, it, beforeAll } from '@jest/globals';
 import { act, render, waitFor } from '@testing-library/react';
 import Board from './Board';
-import { getBoards, setFail } from '../../api/boardsApi';
+import { setFail } from '../../api/boardsApi';
 
 jest.mock('../../api/boardsApi');
 
