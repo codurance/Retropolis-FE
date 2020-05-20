b77b7be8b64bec3e3267c9c44e356f0ee14c4edd
Tom <thomas.spencer@codurance.com>
2020-05-20 09:54:53 +0100

    Refactor: delete comment for localhost

--- src/api/boardsApi.js
+++ src/api/boardsApi.js
@@ -6,7 +6,6 @@ function handleResponse(response) {
 }
 
 async function getBoards() {
-  // const response = await fetch('http://localhost:3000/src/api/boardsApiMock.json');
   const response = await fetch(`${process.env.API_URL}/board`);
   return handleResponse(response);
 }
