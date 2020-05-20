7b1f23ce6c40bb48bb2d7b1a4a14facf7ad5bec4
Tom <thomas.spencer@codurance.com>
2020-05-20 10:29:55 +0100

    Add beforeEach

--- src/components/Board/Board.test.js
+++ src/components/Board/Board.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { jest, it, beforeAll } from '@jest/globals';
+import { jest, it, beforeEach } from '@jest/globals';
 import { act, render, waitFor } from '@testing-library/react';
 import Board from './Board';
 import { setFail } from '../../api/boardsApi';
 
 jest.mock('../../api/boardsApi');
 
-beforeAll(() => {
+beforeEach(() => {
   setFail(false);
 });
 
