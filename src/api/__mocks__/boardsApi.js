b14a38f3f15ad710d2cfb39791f8b6b521737363
Ewan <ewan.s@cucumberwifi.io>
2020-05-21 13:24:47 +0100

    get boards mock not default

--- src/api/__mocks__/boardsApi.js
+++ src/api/__mocks__/boardsApi.js
@@ -1,7 +1,5 @@
 const { getFail } = require('./mockHelper');
 const dummyData = require('./boardsApiMockResponse.json');
 
-export default async function getBoards() {
-  return getFail() ? Promise.reject(new Error('fail'))
-    : new Promise((resolve) => resolve(dummyData));
-}
+export const getBoards = async () => (getFail() ? Promise.reject(new Error('fail'))
+  : new Promise((resolve) => resolve(dummyData)));
