967d861763bdc4f8df9e9dd0d19a95b3c3c35277
Ewan <ewan.s@cucumberwifi.io>
2020-05-26 15:11:28 +0100

    upvote tests

--- src/components/CardItem/CardItem.test.js
+++ src/components/CardItem/CardItem.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { afterEach, it } from '@jest/globals';
-import { cleanup, render } from '@testing-library/react';
+import { afterEach, expect, it } from '@jest/globals';
+import {
+  act, cleanup, render, waitFor
+} from '@testing-library/react';
+import { toBeDisabled } from '@testing-library/jest-dom/matchers';
 import CardItem from './CardItem';
 
 afterEach(cleanup);
@@ -26,4 +29,30 @@ it('shows the user name on the card', () => {
 it('shows a number for the amount of votes', () => {
   const { getByText } = renderCardItem();
   getByText('0');
+});
+
+it('has an up-vote button', () => {
+  const { getByRole } = renderCardItem();
+  getByRole('button');
+});
+
+it('increments the counter when you click up-vote', async () => {
+  await act(async () => {
+    const { getByRole, getByText } = renderCardItem();
+    getByRole('button').click();
+    await waitFor(() => {
+      getByText('1');
+    });
+  });
+});
+
+it('disables the button when you click up-vote', async () => {
+  expect.extend({ toBeDisabled });
+  await act(async () => {
+    const { getByRole } = renderCardItem();
+    getByRole('button').click();
+    await waitFor(() => {
+      expect(getByRole('button')).toBeDisabled();
+    });
+  });
 });
