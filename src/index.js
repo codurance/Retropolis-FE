94c50e6d00e170cf33d7de2131afd50f7aed8082
Ewan <ewan.s@cucumberwifi.io>
2020-06-02 17:32:45 +0100

    route to board with react-router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../public/index.css';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 import Login from './components/Login/Login';
 import NavBar from './components/NavBar/NavBar';
@@ -8,14 +9,16 @@ import Home from './components/Home/Home';
 
 ReactDOM.render(
   <React.StrictMode>
-    <NavBar />
-    <Router>
-      <div>
-        <Route exact path="/" component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/:id" component={Board} />
-      </div>
-    </Router>
+    <div className="App">
+      <NavBar />
+      <Router>
+        <div>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/:id" component={Board} />
+        </div>
+      </Router>
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
