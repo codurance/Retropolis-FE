d953dffa18ac5b25daebbc555122c965fe7784c2
Morinslash <sebastian.elf.kulinski@gmail.com>
2020-06-04 17:05:18 +0200

    refactor on card and board api test

--- src/api/boardsApi.test.js
+++ src/api/boardsApi.test.js
@@ -2,10 +2,11 @@ import {
   expect, it, describe, jest, beforeEach, afterEach
 } from '@jest/globals';
 import { saveBoard, getBoard, getBoards } from './boardsApi';
-
 import * as fetchApi from './fetchApi';
 
 let fetchApiSpy;
+const endpoint = '/boards';
+const invalidMessage = 'Invalid board request';
 
 beforeEach(() => {
   fetchApiSpy = jest.spyOn(fetchApi, 'fetchWrapper').mockImplementationOnce(() => {});
@@ -20,7 +21,7 @@ describe('saveBoard', () => {
     it('throws an error when title doesn\'t exist', (done) => {
       expect.assertions(1);
       saveBoard({ }).catch((err) => {
-        expect(err.message).toEqual('Invalid board request');
+        expect(err.message).toEqual(invalidMessage);
         done();
       });
     });
@@ -28,7 +29,7 @@ describe('saveBoard', () => {
     it('throws an error when title is empty', (done) => {
       expect.assertions(1);
       saveBoard({ title: '' }).catch((err) => {
-        expect(err.message).toEqual('Invalid board request');
+        expect(err.message).toEqual(invalidMessage);
         done();
       });
     });
@@ -41,7 +42,7 @@ describe('saveBoard', () => {
       const body = { title: 'retro 1/1/01', userEmail: 'john.doe@codurance.com' };
       await saveBoard(body);
       expect(fetchApiSpy).toHaveBeenCalledWith({
-        endpoint: '/boards',
+        endpoint,
         method: 'POST',
         body
       });
@@ -55,7 +56,7 @@ describe('getBoard', () => {
     const boardId = 1;
     await getBoard(boardId);
     expect(fetchApiSpy).toHaveBeenCalledWith({
-      endpoint: '/boards/' + boardId
+      endpoint: endpoint + '/' + boardId
     });
   });
 });
@@ -65,7 +66,7 @@ describe('getBoards', () => {
     fetchApiSpy = jest.spyOn(fetchApi, 'fetchWrapper').mockImplementationOnce(() => {});
     await getBoards();
     expect(fetchApiSpy).toHaveBeenCalledWith({
-      endpoint: '/boards'
+      endpoint
     });
   });
 });
